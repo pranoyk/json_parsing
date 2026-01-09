8fe7fa995831ef4c7a3802fecc7c90fcf6e608f2
Pranoy Kumar Kundu <pranoy1998k@gmail.com>
2026-01-09 20:32:39 +0530

    parse number

--- src/parse.rs
+++ src/parse.rs
@@ -8,7 +8,7 @@ fn parse_tokens(tokens: &[Token], index: &mut usize) -> Result<Value, TokenParse
         Token::Null => Ok(Value::Null),
         Token::False => Ok(Value::Boolean(false)),
         Token::True => Ok(Value::Boolean(true)),
-        Token::Number(number) => todo!(),
+        Token::Number(number) => Ok(Value::Number(*number)),
         Token::String(string) => todo!(),
         Token::LeftBracket => todo!(),
         Token::LeftBrace => todo!(),
@@ -50,6 +50,16 @@ mod tests {
     fn parses_false() {
         let input = [Token::False];
         let expected = Value::Boolean(false);
+
+        let actual = parse_tokens(&input, &mut 0).unwrap();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn parse_number() {
+        let input = [Token::Number(10.0)];
+        let expected = Value::Number(10.0);
 
         let actual = parse_tokens(&input, &mut 0).unwrap();
 
